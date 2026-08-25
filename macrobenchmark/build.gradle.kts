15d94f9198b78c6842a391828a27286d5f3a865e
fxcsamis <fxcsamis@gmail.com>
2026-08-25 07:51:41 +0600

    Fix duplicate Kotlin plugin registration in macrobenchmark module

--- macrobenchmark/build.gradle.kts
+++ macrobenchmark/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.test)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -33,9 +32,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
 }
 
